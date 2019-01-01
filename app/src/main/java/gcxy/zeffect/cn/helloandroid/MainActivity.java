26e7b5bc5fd7993609ffa9fa60987cad7816d346
zeffect <625417632@qq.com>
2019-01-01 11:06:23 +0800

    Initial commit

---
+++ app/src/main/java/gcxy/zeffect/cn/helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package gcxy.zeffect.cn.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
